e1c49394c44e6f1e3e08950f12b9a42bd8199900
somyakant9 <msomya143@gmail.com>
2024-05-19 15:57:54 +0530

    removed unneccessary code

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -41,63 +41,6 @@ const accountSlice = createSlice({
   },
 });
 
-// function acccountReducer(state = initialStateAccount, action) {
-//   switch (action.type) {
-//     case "account/deposit":
-//       return {
-//         ...state,
-//         isLoading: false,
-//         balance: state.balance + (+action.payload),
-//       };
-//     case "account/convertingCurrency":
-//       return { ...state, isLoading: true };
-//     case "account/withdraw":
-//       return { ...state, balance: state.balance - (+action.payload) };
-//     case "account/requestLoan":
-//       if (state.loan > 0) return state;
-//       return {
-//         ...state,
-//         loan: action.payload.amount,
-//         balance: state.balance + action.payload.amount,
-//         loanPurpose: action.payload.purpose,
-//       };
-//     case "account/payLoan":
-//       if(state.loan>state.balance) return state;
-//       return {
-//         ...state,
-//         loan: 0,
-//         loanPurpose: "",
-//         balance: state.balance - state.loan,
-//       };
-//     default:
-//       return state;
-//   }
-// }
-
-// export function deposit(amount, currency) {
-//   if (currency === "INR") return { type: "account/deposit", payload: amount };
-
-//   return async function (dispatch, getState) {
-//     dispatch({ type: "account/convertingCurrency" });
-//     //API call
-//     const res = await fetch(
-//       `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=INR`
-//     );
-//     const data = await res.json();
-//     const converted = data.rates.INR;
-//     dispatch({ type: "account/deposit", payload: converted });
-//   };
-// }
-// export function withdraw(amount) {
-//   return { type: "account/withdraw", payload: amount };
-// }
-// export function requestLoan(amount, purpose) {
-//   return { type: "account/requestLoan", payload: { amount, purpose } };
-// }
-// export function payLoan() {
-//   return { type: "account/payLoan" };
-// }
-
 export function deposit(amount, currency) {
     if (currency === "INR") return { type: "account/deposit", payload: amount };
   
